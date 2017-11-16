e09cde43f1ca7431c975b779534dfa97770dbc5d
Andy <smrtwater@126.com>
2017-11-16 23:21:28 +0800

    41tip_switf_object-c

---
+++ language/第三方/SwiftTips/40_条件编译/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  40_条件编译
+//
+//  Created by yhp on 2017/11/16.
+//  Copyright © 2017年 YouHuaPei. All rights reserved.
+//
+
+import Foundation
+
+/***
+ os(macOS/iOS/tvOS/watchOS/Linux)
+ arch(x86_64/arm/arm64/i386)
+ swift(>= N)
+ */
+
+//#if os(macOS)
+//    typealias Color = NSColor
+//#else
+//    typealias Color = UIColor
+//#endif
+/*********注释***********/
+//MARK:
+//TODO:
+//FIXME:
+/*********警告*************/
+//#warning
+
+
+
+
+
