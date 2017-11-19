297d24f60e0fe21692abf27e82bc4ca198c59f9b
Andy <smrtwater@126.com>
2017-11-19 22:36:50 +0800

    51tip_getClass

---
+++ language/第三方/SwiftTips/51_getClass.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+let date = NSDate()
+let name = type(of:date)
+print(name)
+
+let string = "Hello"
+let name2 = type(of:string)
+debugPrint(name2)
+
+
+
