0f5a9b14268d3a208d7a12ff957dbb45cd018c92
ALanHua <yhp-2009@qq.com>
2018-09-08 10:17:54 +0800

    AdvanceSwift_02_03

--- language/AdvanceSwift/AdvanceSwift_02/main.swift
+++ language/AdvanceSwift/AdvanceSwift_02/main.swift
@@ -71,4 +71,78 @@ print(i1.next()!)
 print(i1.next()!)
 print(i2.next()!)
 print(i2.next()!)
+print("----------------")
+// AnyIterator 引用语义
+var i3 = AnyIterator(i1)
+var i4 = i3
+print(i3.next()!)
+print(i4.next()!)
+print(i3.next()!)
+print(i3.next()!)
+// AnyIterator 初始化方法可以直接传入一个next函数
+func fibsIterator() -> AnyIterator<Int> {
+    var state = (0,1)
+    return AnyIterator{
+        let upcomingNumber = state.0
+        state = (state.1,state.0 + state.1)
+        return upcomingNumber
+    }
+}
+
+let fibsSequence = AnySequence(fibsIterator())
+print(Array(fibsSequence.prefix(10)))
+
+// 不稳定序列
+let standardIn = AnySequence {
+    return AnyIterator {
+        readLine()
+    }
+}
+
+let numberedStdIn = standardIn.enumerated()
+//for(i,line) in numberedStdIn {
+//    print("\(i+1):\(line)")
+//}
+
+// 序列和迭代器之间的关系
+/**
+ 其实对于所有的迭代器来说,都可以将他们看作是即将返回的元素所组成
+ 的不稳定序列
+ */
+extension Sequence where Element:Equatable,
+    SubSequence:Sequence,
+    SubSequence.Element == Element{
+    func headMirrorsTail(_ n:Int) -> Bool {
+        let head = prefix(n)
+        let tail = suffix(n).reversed()
+        return head.elementsEqual(tail)
+    }
+}
+
+let equalSeq = [1,2,3,4,2,1].headMirrorsTail(2)
+print(equalSeq)
+
+// 链表
+// note:indirect 在这里是告诉编译器这个媒介的node 应该被看作引用
+enum List<Element> {
+    case end
+    indirect case node(Element,next:List<Element>)
+}
+extension List {
+    func cons(_ x:Element) -> List {
+        return .node(x,next:self)
+    }
+}
+let emptyList = List<Int>.end
+let oneElementList = List.node(1, next: emptyList)
+let list = List<Int>.end.cons(1).cons(2).cons(3)
+print(list)
+// 链式的链表看上去很丑
+
+
+
+
+
+
+
 
