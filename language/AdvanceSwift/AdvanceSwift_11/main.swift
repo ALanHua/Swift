c3511881168f07bf04d8a5a873e2d02fbf81ff4d
ALanHua <yhp-2009@qq.com>
2018-11-15 23:23:47 +0800

    AdvanceSwift11_02

--- language/AdvanceSwift/AdvanceSwift_11/main.swift
+++ language/AdvanceSwift/AdvanceSwift_11/main.swift
@@ -51,4 +51,46 @@ struct ConstantIterator:IteratorProtocol {
 }
 
 // 带有Self的协议
+func f<C:CustomStringConvertible>(_ x:C) -> Int {
+    return MemoryLayout.size(ofValue: x)
+}
+func g(_ x:CustomStringConvertible) -> Int {
+    return MemoryLayout.size(ofValue: x)
+}
+print(f(5))
+print(g(5))
 
+// 互用性
+/***
+ UnsafeMutableRawBufferPointer
+ 含有managed的类型代表内存是自动管理的,编译器将负责为你申请和是否内存
+ 含unsafe类型不提供内存管理
+ 含有buffer类型表示作用于一连串的多个元素
+ raw类型包含无类型的原始数据等同于void*
+ mutable类型允许它指向的内存发生改变
+ 
+ 指针
+ UnsafePointer是最基础的指针类型 --- const指针类似
+    UnsafePointer<Int> --- const int*
+ 
+ */
+var x = 5
+func fetch(p:UnsafePointer<Int>) ->Int {
+    return p.pointee
+}
+
+print(fetch(p: &x))
+
+func increent(p:UnsafeMutablePointer<Int>){
+    p.pointee += 1
+}
+increent(p: &x)
+print(x)
+
+// 手动申请内存
+let z = UnsafeMutablePointer<Int>.allocate(capacity: 2)
+z.initialize(repeating: 42, count: 2)
+print(z.pointee)
+(z+1).pointee = 43
+print(z[1])
+z.deallocate()
---
+++ language/AdvanceSwift/AdvanceSwift_12/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvanceSwift_12
+//
+//  Created by yhp on 2018/11/15.
+//  Copyright © 2018 none. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
